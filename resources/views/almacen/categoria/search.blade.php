7573be986abc6a03564e37af557d32b7660d199b
jdcali <nanielycalisaya@gmail.com>
2018-03-19 20:52:42 -0500

    listado y busqueda de categorias

---
+++ resources/views/almacen/categoria/search.blade.php
@@ -0,0 +1,15 @@
+{!! Form::open(array('url'=>'almacen/categoria','method'=>'GET','autocomplete'=>'on','role'=>'search'))!!}
+
+<div class="form-group">
+	<div class="import-group">
+		<input type="text" class="form-control" name="searchText" placeholder="Buscar..." value="{{$searchText}}">
+		<span class="input-group-btn">
+			<button type="submit" class="btn btn-primary">Buscar</button>			
+		</span>
+		
+
+		
+	</div>
+	</div>
+
+{{Form::close()}}
