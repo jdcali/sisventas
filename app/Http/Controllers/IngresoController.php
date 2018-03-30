e0b718389654e2a8ca0ef8fc1ec0ce997405c9aa
jdcali <nanielycalisaya@gmail.com>
2018-03-29 19:24:07 -0500

    subiendo modulo de ingreso con consultas funcionando correctamente

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -28,14 +28,14 @@ class IngresoController extends Controller
         {
             $query=trim($request->get('searchText'));
             $ingresos=DB::table('ingreso as i')
-                ->join('persona asp','i.proveedor','=','p.idpersona')
-                ->join('detalle_ingreso as di','i.idingreso','='.'di.ingreso')
+                ->join('persona as p','i.idproveedor','=','p.idpersona')
+                ->join('detalle_ingreso as di','i.idingreso','=','di.idingreso')
                 ->select('i.idingreso','i.fecha_hora','p.nombre','i.tipo_comprobante','i.serie_comprobante','i.num_comprobante','i.impuesto','i.estado',DB::raw('sum(di.cantidad*precio_compra)as total'))
                 ->where('i.num_comprobante','LIKE','%'.$query.'%')
                 ->orderBy('i.idingreso','desc')
                 ->groupBy('i.idingreso','i.fecha_hora','p.nombre','i.tipo_comprobante','i.serie_comprobante','i.num_comprobante','i.impuesto','i.estado')
                 ->paginate(7);
-                return view('compras.ingreso.index',["ingresos"=>$ingresos,"searchText"=$query]);
+                return view('compras.ingreso.index',["ingresos"=>$ingresos,"searchText"=>$query]);
          }
         }
     public function create(){
@@ -52,11 +52,69 @@ class IngresoController extends Controller
     {
         try {
             DB::beginTransaction();
+            $ingreso=new Ingreso;
+            $ingreso->idproveedor=$request->get('idproveedor');
+            $ingreso->tipo_comprobante=$request->get('tipo_comprobante');
+            $ingreso->serie_comprobante=$request->get('serie_comprobante');
+            $ingreso->num_comprobante=$request->get('num_comprobante');
+
+            $mytime = Carbon::now('America/Lima');
+            $ingreso->fecha_hora=$mytime->toDateTimeString();
+            $ingreso->impuesto='18';
+            $ingreso->estado='A';
+            $ingreso->save();
+
+            $idarticulo = $request->get('idarticulo');
+            $cantidad = $request->get('cantidad');
+            $precio_compra = $request->get('precio_compra');
+            $precio_venta = $request->get('precio_venta');
+
+            $cont = 0;
+
+            while ( $cont <= count($idarticulo)) {
+                $detalle= new DetalleIngreso();
+                $detalle->idingreso=$ingreso->idingreso;
+                $detalle->idarticulo=$idarticulo[$cont];
+                $detalle->cantidad= $cantidad[$cont];
+                $detalle->precio_compra= $precio_compra[$cont];
+                $detalle->precio_venta= $precio_venta[$cont];
+                $detalle->save();
+                $cont =$cont+1;
+            }
 
             DB::commit();
 
         } catch (Exception $e) {
             DB::rollback();
         }
+
+        return Redirect::to('compras/ingreso');
+    }
+
+    public function show($id)
+    {
+
+        $ingreso=DB::table('ingreso as i')
+                ->join('persona asp','i.proveedor','=','p.idpersona')
+                ->join('detalle_ingreso as di','i.idingreso','='.'di.ingreso')
+                ->select('i.idingreso','i.fecha_hora','p.nombre','i.tipo_comprobante','i.serie_comprobante','i.num_comprobante','i.impuesto','i.estado',DB::raw('sum(di.cantidad*precio_compra)as total'))
+                ->where('i.idingreso','=',$id)
+                ->first();
+
+        $detalles=DB::table('detalle_ingreso as d')
+        ->join('articulo as a', 'd.idarticulo','=','a.articulo')
+        ->select('a.nombre as articulo','d.cantidad','d.precio_compra','d.precio_venta')
+        ->where('d.idingreso','=',$id)
+        ->get();
+
+        return view("compras.ingreso.show",["ingreso"=>$ingreso,"detalles"=>$detalles]);
+    }
+
+    public function destroy($id)
+    {
+        $ingreso=Ingreso::FindOrFail($id);
+        $ingreso->Estado='C';
+        $ingreso->update();
+        return Redirect::to('compras/ingreso');
     }
 }
